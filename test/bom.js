b993f0b3131480e8e5f2ddc4704b649f29d72557
Anthony Beaumont <dev.xan105@outlook.be>
2021-11-13 17:29:30 +0700

    Update bom.js

--- test/bom.js
+++ test/bom.js
@@ -1,7 +1,24 @@
 import * as fs from "../lib/esm.js";
+import { readFile } from "node:fs/promises";
 
 await fs.writeFile("./sample/test.txt","hello world", { bom: true, encoding: "utf16le" });
 const txt = await fs.readFile("./sample/test.txt","utf16le");
 console.log(txt);
+
+await fs.writeFile("./sample/test.json",JSON.stringify({hello: "world"}, null, 2), { bom: true, encoding: "utf8" }); //doesn't respect json spec
+await fs.writeFile("./sample/test2.json",JSON.stringify({hello: "world"}, null, 2), { encoding: "utf8" });
+const buffer = await fs.readFile("./sample/test.json");
+const buffer2 = await fs.readFile("./sample/test2.json");
+console.log(buffer);
+console.log(buffer2);
+const json = await fs.readJSON("./sample/test.json");
+console.log(json);
+
+try{
+  const json2 = JSON.parse( await readFile("./sample/test.json", "utf8") );
+  console.log(json2);
+}catch(e){
+  console.log(`expected error: ${e}`);
+}
 
 await fs.rmdir("./sample"); //clean
