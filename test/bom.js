663c8b4495f9c1c00f02665d8dd581dd8647ad7f
Anthony Beaumont <dev.xan105@outlook.be>
2022-09-18 21:23:52 +0700

    sync

--- test/bom.js
+++ test/bom.js
@@ -6,13 +6,12 @@ t.test("bom", async t => {
 
 await fs.writeFile("./test/sample/test.txt", "hello world", { bom: true, encoding: "utf16le" });
 const txt = await readFile("./test/sample/test.txt", "utf16le");
-const txt2 = await fs.readFile("./test/sample/test.txt", { encoding: "utf16le", bomWarning: false });
+const txt2 = await fs.readFile("./test/sample/test.txt", { encoding: "utf16le" });
 
-t.ok(typeof txt === "string" && typeof txt2 === "string");
-t.ok(txt.charCodeAt(0) === 0xFEFF);
-t.ok(txt2.charCodeAt(0) !== 0xFEFF);
+t.ok(typeof txt === "string" && txt.charCodeAt(0) === 0xFEFF);
+t.ok(typeof txt2 === "string" && txt2.charCodeAt(0) !== 0xFEFF);
 
-await fs.rmdir("./test/sample"); //clean
+await fs.rm("./test/sample"); //clean
 
 t.end();
 });
